4474602f50d546f9ad25f89013e2491175075e28
ghaniusman <mail.to.usmanghani@gmail.com>
2022-05-17 14:38:06 +0100

    adding files

---
+++ mario1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void){
+int m = 0;
+int n;
+
+   do{
+       printf("\npleae enter height between 1 to 8\n\n");
+       scanf("%d",&m);
+       n = m;
+       
+   
+      for(int i= 0 ; i<n  ; i++  ){
+         for(int k = i; k<=n-2  ; k++  ){
+            printf(" ");
+         }
+         for(int j = 0; j<i+1; j++ ){
+            printf("#");
+         }
+         printf("\n");
+      }
+   }while(n>0 && n<=8);
+}
